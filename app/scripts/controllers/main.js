e32d5f5f037ff83608ac68112a5f4cdba48684b3
Rahul Pisharody <rpisharody@gmail.com>
2013-07-02 12:00:24 -0700

    Barebones
    Built a very basic Feed Reader which shows the Feed from DailyJS
    Bad Formatting and stuff !

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('vayanaApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fdailyjs.com%2Fatom.xml'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+
+    $http.jsonp(url).success(function(data, status, headers, config) {
+        $scope.feed = {
+            title: 'DailyJS',
+            items: data.query.results.entry
+        };
+    }).error(function(data, status, headers, config) {
+        console.error('Error fetching feed:', data);
+    });
+  });
